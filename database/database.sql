a4b2a905085bce146587e80da54d5adeba62b7cd
mferreira96 <pount@sapo.pt>
2016-11-15 23:59:57 +0000

    starting database

---
+++ database/database.sql
@@ -0,0 +1,25 @@
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  firstName TEXT,
+  lastName TEXT,
+  email TEXT,
+  photo TEXT,
+  password TEXT,
+);
+
+
+CREATE TABLE restaurant (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  description TEXT,
+  rate INTEGER,
+  photo TEXT,
+);
+
+
+CREATE TABLE review (
+  id INTEGER PRIMARY KEY
+  rate INTEGER,
+  comment TEXT,
+
+);
