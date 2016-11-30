b702c30d422f08632b31817cf2ed1377461be1f8
Carmen Eitel <carmen.eitel@yahoo.de>
2016-11-30 11:23:19 +0000

    update

---
+++ owner/owner_menu.php
@@ -0,0 +1,9 @@
+<?php
+  include_once(../header.php);
+?>
+  <div id="menu_owner">
+    <ul>
+      <li><a>My Restaurants</a></li>    <!-- inserting an restaurant a "sub-li" should be created -->
+      <li><a>Logout</a></li>
+    </ul>
+  </div>
