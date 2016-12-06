36226c81695b09a7427e4cc429247df8e637cd11
mferreira96 <pount@sapo.pt>
2016-12-06 23:23:45 +0000

    now uploads an image to right dir

--- actions/add_restaurant.php
+++ actions/add_restaurant.php
@@ -1,19 +1,30 @@
 <?php
 
-
   include_once('../database/connection.php');
   include_once('../database/restaurant.php');
   include_once('../database/image.php');
 
+  //session_start();
 
   $variables = $_POST;
-
+  $uploads_dir = "../pics";
   try {
-    echo $_POST['restaurant_picture'];
-    addImage($_POST['restaurant_picture']);
+
+    $tmp_name = $_FILES["restaurant_picture"]["tmp_name"];
+    $name = basename($_FILES["restaurant_picture"]["name"]);
+    addImage($name);
 
     $id= $dbh->lastInsertId();
+
+    move_uploaded_file($tmp_name, "$uploads_dir/$name");
+
     addRestaurant($variables, $id);
+    $idRestaurant= $dbh->lastInsertId();
+/*
+  Para quando ja estiver a funcionar o user
+
+    addOwner($idRestaurant, $_SESSION['username']);
+*/
   } catch (PDOException $e) {
     die($e->getMessage());
   }
