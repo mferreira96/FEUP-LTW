8090c14111c4d60b86722502f07562fa0eaedaa5
João Castro <up201404896@fe.up.pt>
2016-12-07 00:51:54 +0000

    improvements on login

---
+++ templates/login.php
@@ -0,0 +1,13 @@
+<div id="login_message" class="login_message">
+	
+	<h1>Login</h1>
+	
+	<form action ="../actions/login.php" method="post" enctype="multipart/form-data">
+	<div id="login_form">
+		<label for ="username">Username:</label>
+		<input type="text" name="username" id ="username" required="required">
+		<br>
+		<label for ="password">Password: </label>
+		<input type="text" name="password" id ="password" required="required">
+	</div>
+</div>
