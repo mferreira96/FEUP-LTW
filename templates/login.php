12fe076dea632f3906b6ffd9657df56223ccb0b1
João Castro <up201404896@fe.up.pt>
2016-12-07 09:49:52 +0000

    update

--- templates/login.php
+++ templates/login.php
@@ -3,11 +3,13 @@
 	<h1>Login</h1>
 	
 	<form action ="../actions/login.php" method="post" enctype="multipart/form-data">
-	<div id="login_form">
-		<label for ="username">Username:</label>
-		<input type="text" name="username" id ="username" required="required">
-		<br>
-		<label for ="password">Password: </label>
-		<input type="text" name="password" id ="password" required="required">
-	</div>
+		<div id="login_form">
+			<label for ="username">Username:</label>
+			<input type="text" name="username" id ="username" required="required">
+			<br>
+			<label for ="password">Password: </label>
+			<input type="text" name="password" id ="password" required="required">
+		</div>
+		<input type="submit" value="Login"
+	</form>
 </div>
