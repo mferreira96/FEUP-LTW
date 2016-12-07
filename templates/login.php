02c0de1e728254189772251ae6b92180df7e4155
mferreira96 <pount@sapo.pt>
2016-12-07 10:17:57 +0000

    small change

--- templates/login.php
+++ templates/login.php
@@ -1,14 +1,14 @@
 <div id="login_message" class="login_message">
-	
+
 	<h1>Login</h1>
-	
+
 	<form action ="../actions/login.php" method="post" enctype="multipart/form-data">
 		<div id="login_form">
 			<label for ="username">Username:</label>
 			<input type="text" name="username" id ="username" required="required">
 			<br>
 			<label for ="password">Password: </label>
-			<input type="text" name="password" id ="password" required="required">
+			<input type="password" name="password" id ="password" required="required">
 		</div>
 		<input type="submit" value="Login"
 	</form>
