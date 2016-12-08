33d1c0c670aac924160bf0149c23ac334b753d87
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-08 13:47:44 +0000

    gallery
    gallery works with example pictures, code could be separated better
    (javascript in html)

--- templates/restaurant/restaurant_tabs/restaurant_gallery.php
+++ templates/restaurant/restaurant_tabs/restaurant_gallery.php
@@ -1,2 +1,21 @@
 <div id="restaurant_gallery" class="restaurant_tabs_content">
+  <!--<div class="restaurant_photo_gallery"> <-->
+  <div class="pictures_by_reviewers">
+    <img onmouseover="pic_preview.src=img1.src" name="img1" src="../pics/food_1.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img2.src" name="img2" src="../pics/food_2.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img3.src" name="img3" src="../pics/food_3.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img4.src" name="img4" src="../pics/food_4.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img5.src" name="img5" src="../pics/food_5.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img6.src" name="img6" src="../pics/food_1.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img7.src" name="img7" src="../pics/food_2.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img8.src" name="img8" src="../pics/food_3.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img9.src" name="img9" src="../pics/food_4.jpg" alt=""/>
+    <img onmouseover="pic_preview.src=img10.src" name="img10" src="../pics/food_5.jpg" alt=""/>
+  </div><br/>
+
+    <div class="pic_preview">
+      <img id="pic_preview" src="../pics/food_1.jpg" alt=""/>
+    </div>
+
+  <!--</div>-->
 </div>
