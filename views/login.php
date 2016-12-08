37011e407b14b7cfe28b67c618a3f6454aff4128
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-08 10:58:42 +0000

    login and form css improvements

---
+++ views/login.php
@@ -0,0 +1,7 @@
+<?php
+  include_once('../templates/header.php');
+  echo '<div class="content">';
+    include_once('../templates/login.php');
+  echo '</div>';
+  include_once('../templates/footer.php');
+?>
