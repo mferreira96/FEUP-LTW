7927155d12e07fe020fd12bef446c097139b4a58
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-11 22:55:23 +0000

    change of actions login
    userExists() has to be implemented

---
+++ actions/login.php
@@ -0,0 +1,18 @@
+<?php
+//session_start();
+if(isset($_POST['username']) && isset($_POST['password'])) //websecurity, key!
+{
+  include_once('../database/connection.php');
+  include_once('../database/user.php');
+  if (true || userExists($dbh, $_POST['username'], $_POST['password'])){   //delete true|| and write the function
+      $_SESSION['username'] = $_POST['username'];
+      $_SESSION['user-logged'] = true;
+      header('Location: ../public/restaurant_overview.php');
+  }else{
+    header('Location: ../public/login.php');
+  }
+}else{
+  header('Location: ../public/login.php');
+}
+//header('Location: ' . $_SERVER['HTTP_REFERER']);
+?>
