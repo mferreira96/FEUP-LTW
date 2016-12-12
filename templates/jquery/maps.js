e5755839a40817af849d7ee3b8ed013a80b90c10
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-12 23:14:52 +0000

    google maps integration

--- templates/jquery/maps.js
+++ templates/jquery/maps.js
@@ -1,11 +1,33 @@
+var geocoder;
+var map;
+
 function initMap() {
        var location = {lat: 41.157944, lng: -8.629105};
-       var map = new google.maps.Map(document.getElementById('maps'), {
+       map = new google.maps.Map(document.getElementById('maps'), { //var map
          zoom: 8,
          center: location
        });
-       var marker = new google.maps.Marker({
-         position: location,
-         map: map
-       });
-     }
+
+
+       geocoder = new google.maps.Geocoder();
+       codeAddress();
+}
+
+function codeAddress() {
+    var address = document.getElementById("map_adress").value;
+    // var address_street_nr = 'Rohrackkerstr 154';      //load
+    // var address_city_postcode = 'Stuttgart 70329';   //load
+    // var address = address_street_nr + ' ' + address_city;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        map.setZoom(12);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
--- views/restaurant.php
+++ views/restaurant.php
@@ -8,7 +8,12 @@
 
   $id = $_GET['id'];
   $restaurant = getRestaurantById($id);
+  $address_street_nr = $restaurant['street'].' '.$restaurant['number'];
+  $address_city_postcode = $restaurant['city'].' '.$restaurant['postcode'];
 
+  echo '<input id="map_adress" type="hidden" value="';
+  echo $address_street_nr." ";
+  echo $address_city_postcode."\">";
 
   include_once('../templates/restaurant/restaurant_above_tabs.php');
     echo '<div class="main_menu_oriented">';
