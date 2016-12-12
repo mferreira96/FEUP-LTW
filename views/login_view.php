42c930d02542d2ac844abbbab905b0e7a2dad6bd
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-12 13:41:51 +0000

    Revert "login_view implemented"
    This reverts commit 9bbf89a1fe2a2ab65f4de1889419023d8f67cef5.

--- views/login_view.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include_once('../templates/header.php');
-  echo '<div class="content">';
-      include_once('../templates/login_template.php');
-  echo '</div>';
-  include_once('../templates/footer.php');
-  ?>
