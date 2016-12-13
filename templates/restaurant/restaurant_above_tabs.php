4aa3c299dce228a4a676550e20480026afc49854
mferreira96 <pount@sapo.pt>
2016-12-13 22:29:00 +0000

    fixed image

--- templates/restaurant/restaurant_above_tabs.php
+++ templates/restaurant/restaurant_above_tabs.php
@@ -3,8 +3,13 @@
   <div id="restaurant_name">
     <h1><?= $restaurant['name'] ?></h1>
   </div>
+  <?php
+    include_once('../database/connection.php');
+    include_once('../database/image.php');
 
+    $images =  getImagesOfRestaurant($restaurant['id']);
+   ?>
   <div id="restaurant_picture">
-   <img src="../pics/restaurant_example_2.png" alt="restaurant_picture" width="400" height="200">
+   <img src=<?="../pics/". $images[0]['name'] ?> alt="restaurant_picture" width="400" height="200">
   </div>
 </div>
