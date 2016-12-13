22a042c63997b08e0d13e8b592b0e73a8e952175
João Castro <up201404896@fe.up.pt>
2016-12-13 19:54:52 +0000

    profile picture on reviews working

--- templates/restaurant/restaurant_tabs/restaurant_all_reviews.php
+++ templates/restaurant/restaurant_tabs/restaurant_all_reviews.php
@@ -5,16 +5,20 @@
     include_once('../database/connection.php');
     include_once('../database/restaurant.php');
     include_once('../database/review.php');
+	include_once('../database/image.php');
 
     $reviews =  getAllReviewsFromRestaurant($restaurant['id']);
+	
+
 
 ?>
 
 <section class="listReviews">
      <?php foreach($reviews as $review) { ?>
         <article class="review">
+		  <?php $image = getImageOfUser($review['username']); ?>
           <div id="profile_picture">
-            <img src="../pics/blank-profile-picture.png" alt="profile_picture" width="100" height="100">
+            <img src=<?="../pics/". $image['name'] ?> alt="profile_picture" width="100" height="100">
           </div>
 
           <div id="name">
