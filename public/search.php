4d1557cffa8b22d978e113f319a55cbacdf893ed
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-13 01:44:08 +0000

    changes on normal search

---
+++ public/search.php
@@ -0,0 +1,7 @@
+<?php
+  include('../helpers/userAdministration.php');
+  include('../database/connection.php');
+  include('../database/restaurant.php');
+  login_check();
+  //do I have to include anything?
+?>
--- templates/reviewer/reviewer_menu.php
+++ templates/reviewer/reviewer_menu.php
@@ -1,6 +1,7 @@
 <nav id="menu_reviewer">
   <ul>
-    <li><a><input type="text" id="search_by_name_only" name="search_by_name_only" value="Search by name"><input type="button" id="start_search_by_name_only" value="Search"></a></li>    <!-- searches by name only -->
+    <li><a><form action ="../actions/search_name.php" method="post" enctype="multipart/form-data"><input type="text" id="search_by_name_only" name="search_by_name_only" placeholder="Search by name"></a><input type="submit" value="Search">
+	</form></li>    <!-- searches by name only --> <!--<input type="button" id="start_search_by_name_only" value="Search">-->
     <li><a>Advanced search</a></li>  <!-- <href="ADVANCED SEARCH"> -->
     <li><a href="edit_user.php">Edit profile</a></li>
     <li><a href="../actions/change_userType.php">Become owner</a></li>
