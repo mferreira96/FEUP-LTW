7b5b3edc62bffa962b9768b5c20408f13a887854
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-13 19:39:55 +0000

    review reply

--- templates/owner/reply_review.php
+++ templates/owner/reply_review.php
@@ -1,3 +1,4 @@
+<form action="../actions/add_reply.php?id=<?php echo $review['id']; ?>" method="post"  enctype="multipart/form-data">
   <div id="reply_review">
     <h3>Reply to review</h3>
     <textarea id="reply_text" name="comment" rows="8" cols="50">
