80ff94f9ea2a9c341c4732d9f3d4c8241f584fb2
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-13 18:27:33 +0000

    review redirect

--- actions/add_review.php
+++ actions/add_review.php
@@ -6,18 +6,16 @@
 
 if(isset($_POST['submit'])){
   try {
-<<<<<<< HEAD
     addReview($idRestaurant, $username, $rate, $comment);
     //modificar para as labels respetiveas
-=======
+
     print_r($_POST);
     addReview($_GET['id'], $_SESSION['username'], $_POST['rating'], $_POST['comment']);
 
->>>>>>> origin/master
   } catch (PDOException $e) {
     die($e->getMessage());
   }
 }
 
-  header('Location: ../public/restaurant.php');
+  header('Location: ../public/restaurant_overview.php');
 ?>
