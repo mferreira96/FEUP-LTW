f2f2e26d6b48219f974160e52611053dc493d8d6
Carmen Eitel <carmen.eitel@yahoo.de>
2016-12-13 01:24:52 +0000

    advaced search view

--- templates/advanced_search.php
+++ templates/advanced_search.php
@@ -1,25 +1,17 @@
 <div id="advanced_search" class="advanced_search">
 
-	<h3>Advanced Search</h3>
-
 	<form action ="../actions/advanced_search_name.php" method="post" enctype="multipart/form-data">
 		<div id="search_by_name_form">
 			<label for ="username">Name: </label>
 			<input type="text" name="name" id ="name">
 		</div>
-		<input type="submit" value="Search">
-	</form>
-	<form action ="../actions/advanced_search_city.php" method="post" enctype="multipart/form-data">
 		<div id="search_by_name_form">
 			<label for ="city">City: </label>
 			<input type="text" name="city" id ="city">
 		</div>
-		<input type="submit" value="Search">
-	</form>
-	<form action ="../actions/advanced_search_rate.php" method="post" enctype="multipart/form-data">
 		<div id="search_by_name_form">
 			<label for ="limit">Minimum rating: </label>
-			<input type="number" name="limit" id ="limit">
+			<input type="number" min="0" max="5" name="limit" id ="limit" value="0">
 		</div>
 		<input type="submit" value="Search">
 	</form>
